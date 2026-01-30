94a28b45152cc07317b4810fc07ead5ff9f32385
stevegiunta <steve.4gs@gmail.com>
2026-01-29 23:15:40 -0500

    summary

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = 'pizzacalc-v2';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'pizzacalc-v10';
+const ASSETS = ['index.html', 'manifest.json'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
